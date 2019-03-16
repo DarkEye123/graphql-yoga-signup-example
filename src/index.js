084b8e980ac87dc1340dae2e556ae224dbfc2f02
Matej Lesko <lesko.matej.pu@gmail.com>
2019-03-16 21:08:53 +0100

    comment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ server.use(helmet());
 
 server.start(
   {
+    // allow a communication from these urls
     cors: { credentials: true, origin: [process.env.FRONTEND_URL, process.env.BACKED_URL, process.env.PLAYGROUND_URL] },
   },
   (app) => {
